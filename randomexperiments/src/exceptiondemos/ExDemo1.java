a129099e16bf5a12f96958a47b0d236487fc5387
vineetsemwal <vineetsemwal82@gmail.com>
2023-10-06 12:11:50 +0530

    exception demo improved

--- randomexperiments/src/exceptiondemos/ExDemo1.java
+++ randomexperiments/src/exceptiondemos/ExDemo1.java
@@ -4,7 +4,7 @@ public class ExDemo1 {
 
     public static void main(String[] args) {
         ExDemo1 demo=new ExDemo1();
-        String input=null;
+        String input="";
         String result=demo.getBlockLetterString(input);
         System.out.println("input="+input);
         System.out.println("result="+result);
@@ -22,11 +22,24 @@ public class ExDemo1 {
             String desired = capitalCase + input.substring(1);
             System.out.println("returning result");
             return desired;
-        }catch (NullPointerException e){
+        }
+
+        catch (NullPointerException e){
             System.out.println("exception caught,input is null");
             System.out.println("exception msg="+e.getMessage());
            // e.printStackTrace();
             return null;
         }
+        catch (StringIndexOutOfBoundsException e){
+            System.out.println("exception caught, input is empty");
+            System.out.println("exception message="+e.getMessage());
+            return null;
+        }
+        catch (Exception e){
+            System.out.println("somethinng went wrong");
+            System.out.println("exception msg="+e.getMessage());
+            return null;
+        }
+
     }
 }
