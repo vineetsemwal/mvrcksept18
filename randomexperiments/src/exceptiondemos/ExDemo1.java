5f88bc0bce9529eb27ade560c12dd2204b2bc8cb
vineetsemwal <vineetsemwal82@gmail.com>
2023-10-06 13:04:48 +0530

    exception demo , custom exceptions, throw, throws discussed

--- randomexperiments/src/exceptiondemos/ExDemo1.java
+++ randomexperiments/src/exceptiondemos/ExDemo1.java
@@ -3,43 +3,38 @@ package exceptiondemos;
 public class ExDemo1 {
 
     public static void main(String[] args) {
-        ExDemo1 demo=new ExDemo1();
-        String input="";
-        String result=demo.getBlockLetterString(input);
-        System.out.println("input="+input);
-        System.out.println("result="+result);
+        ExDemo1 demo = new ExDemo1();
+        String input = "";
+        try {
+            String result = demo.getBlockLetterString(input);
+            System.out.println("input=" + input);
+            System.out.println("result=" + result);
+            char firstChar = result.charAt(0);
+            System.out.println("input first char =" + firstChar);
+        }catch (InvalidInputException e){
+            System.out.println("input you provided was wrong");
+        }
+        finally {
+            System.out.println("i am always executed");
+        }
     }
 
     /**
-     *
      * return string where first letter is capital
+     * throws InvalidInputException if input is null or blank
      */
-    String getBlockLetterString(String input){
-        System.out.println("inside getBlockLetterString(*)");
-        try {
+    String getBlockLetterString(String input)throws InvalidInputException{
+            System.out.println("inside getBlockLetterString(*)");
+            if (input == null || input.isBlank()) {
+                throw new InvalidInputException("invalid input");
+            }
             char firstChar = input.charAt(0);
             char capitalCase = Character.toUpperCase(firstChar);
             String desired = capitalCase + input.substring(1);
             System.out.println("returning result");
             return desired;
-        }
 
-        catch (NullPointerException e){
-            System.out.println("exception caught,input is null");
-            System.out.println("exception msg="+e.getMessage());
-           // e.printStackTrace();
-            return null;
-        }
-        catch (StringIndexOutOfBoundsException e){
-            System.out.println("exception caught, input is empty");
-            System.out.println("exception message="+e.getMessage());
-            return null;
-        }
-        catch (Exception e){
-            System.out.println("somethinng went wrong");
-            System.out.println("exception msg="+e.getMessage());
-            return null;
-        }
 
     }
+
 }
