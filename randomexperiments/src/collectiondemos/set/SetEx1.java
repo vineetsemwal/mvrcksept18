3c0bdc8feb46d013047b9f9133254e344eeb1e0c
vineetsemwal <vineetsemwal82@gmail.com>
2023-10-10 11:16:24 +0530

    iterator , treeset demo

--- randomexperiments/src/collectiondemos/set/SetEx1.java
+++ randomexperiments/src/collectiondemos/set/SetEx1.java
@@ -3,20 +3,37 @@ package collectiondemos.set;
 import collectiondemos.list.Product;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetEx1 {
     public static void main(String[] args) {
         SetEx1 demo=new SetEx1();
-        Set<Product>products=new HashSet<>();
+        Set<Product>products=new LinkedHashSet<>();
         products.add(new Product(3,"samsung"));
         products.add(new Product(1,"moto"));
         products.add(new Product(5,"iphone"));
         products.add(new Product(5,"iphone"));
         System.out.println("size="+products.size());
-        for (Product iterated:products){
-            demo.display(iterated);
+
+        System.out.println("***displaying all products");
+        for (Product product:products){
+            demo.display(product);
         }
+
+        System.out.println("***removing product with name iphone");
+        //iterating and removing element
+        Iterator<Product>iterator =products.iterator();
+        while (iterator.hasNext()){
+           Product iterated= iterator.next();
+           if(iterated.getName().equals("iphone")) {
+               iterator.remove();
+           }
+        }
+        System.out.println("after removal size="+products.size());
+
+
 
 
 
---
+++ randomexperiments/src/collectiondemos/set/SetEx2.java
@@ -0,0 +1,30 @@
+package collectiondemos.set;
+
+import collectiondemos.list.Product;
+import collectiondemos.list.ProductNameComparatorAscending;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetEx2 {
+    public static void main(String[] args) {
+        SetEx1 demo=new SetEx1();
+        Set<Product> products=new TreeSet<>(new ProductNameComparatorAscending());
+        products.add(new Product(3,"samsung"));
+        products.add(new Product(1,"moto"));
+        products.add(new Product(5,"iphone"));
+        products.add(new Product(5,"iphone"));
+        System.out.println("size="+products.size());
+        for (Product iterated:products){
+            demo.display(iterated);
+        }
+
+
+
+    }
+    void display(Product product){
+        System.out.println("product-"+product.getId()+"-"+product.getName());
+    }
+
+}
