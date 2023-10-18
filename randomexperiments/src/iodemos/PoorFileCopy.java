1630b049e86449a1f8c83819b46dc90851a70f28
vineetsemwal <vineetsemwal82@gmail.com>
2023-10-18 10:35:15 +0530

    poor file copy , streams closed properly

--- randomexperiments/src/iodemos/PoorFileCopy.java
+++ randomexperiments/src/iodemos/PoorFileCopy.java
@@ -4,11 +4,17 @@ import java.io.*;
 
 public class PoorFileCopy {
     public static void main(String[] args) {
+        PoorFileCopy demo=new PoorFileCopy();
+        demo.run();
+    }
+    public void run() {
+        InputStream inputStream=null;
+        OutputStream outputStream=null;
         try {
             File inputFile = new File("C:/Users/vineets/work/ioexp/fruits.txt");
             File outFile=new File("C:/Users/vineets/work/ioexp/fruits-copy.txt");
-            InputStream inputStream = new FileInputStream(inputFile);
-            OutputStream outputStream=new FileOutputStream(outFile);
+            inputStream = new FileInputStream(inputFile);
+            outputStream=new FileOutputStream(outFile);
             int b=-1;
             int bytesCount=0;
             while ((b=inputStream.read())>0){
@@ -16,10 +22,20 @@ public class PoorFileCopy {
                 bytesCount++;
             }
             System.out.println("\nbytes read="+bytesCount);
-            inputStream.close();
-            outputStream.close();
+
         } catch (IOException e) {
             System.out.println("exception in reading");
+            e.printStackTrace();
+        }
+        finally {
+            close(inputStream);
+            close(outputStream);
+        }
+    }
+    void close(Closeable stream){
+        try {
+            stream.close();
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
