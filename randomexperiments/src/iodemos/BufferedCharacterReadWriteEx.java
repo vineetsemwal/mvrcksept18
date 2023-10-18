8a40bd57fe653cc83df7cb3c17a082cd2261cc68
vineetsemwal <vineetsemwal82@gmail.com>
2023-10-18 10:55:25 +0530

    try with resources used in remaining classes

--- randomexperiments/src/iodemos/BufferedCharacterReadWriteEx.java
+++ randomexperiments/src/iodemos/BufferedCharacterReadWriteEx.java
@@ -4,22 +4,17 @@ import java.io.*;
 
 public class BufferedCharacterReadWriteEx {
     public static void main(String[] args) {
-        try{
+        try(
             Reader reader=new InputStreamReader(System.in);
             BufferedReader bufferedReader=new BufferedReader(reader);
             Writer writer=new OutputStreamWriter(System.out);
             BufferedWriter bufferedWriter=new BufferedWriter(writer);
+        ){
             String line;
             while (!(line=bufferedReader.readLine()).equals("exit")){
               bufferedWriter.write(line);
             }
             bufferedWriter.flush();
-
-            bufferedReader.close();
-            reader.close();
-            bufferedWriter.close();
-            writer.close();
-
 
         }catch (IOException e){
             e.printStackTrace();
