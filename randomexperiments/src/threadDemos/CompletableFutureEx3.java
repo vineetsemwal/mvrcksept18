e805b52e1b26d9fe8b859e15fec6ee53f4d306de
vineetsemwal <vineetsemwal82@gmail.com>
2023-10-19 15:15:14 +0530

    exception handling future

---
+++ randomexperiments/src/threadDemos/CompletableFutureEx3.java
@@ -0,0 +1,30 @@
+package threadDemos;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
+public class CompletableFutureEx3 {
+    public static void main(String[] args)throws Exception {
+        CompletableFuture<String>future=CompletableFuture.supplyAsync(new OriginalTask());
+        CompletableFuture<String>resultFuture=future.handleAsync(new HandlerTask());
+        String result=resultFuture.get();
+        System.out.println(result);
+    }
+    private static class HandlerTask implements BiFunction<String,Throwable,String>{
+        @Override
+        public String apply(String result, Throwable exception) {
+            if (exception!=null){
+                return exception.getMessage()+"-something went wrong";
+            }
+            return result;
+        }
+    }
+    private static class OriginalTask implements Supplier<String>{
+        @Override
+        public String get() {
+          // return "Mumbai";
+            throw new RuntimeException("originaltask threw exception");
+        }
+    }
+}
