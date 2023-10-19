a1e919f5ac7ed82bf8be5ebfa8462c8e2c7a7cdc
vineetsemwal <vineetsemwal82@gmail.com>
2023-10-19 14:43:49 +0530

    completable demo-alternate result

---
+++ randomexperiments/src/threadDemos/CompleteableFutureEx1.java
@@ -0,0 +1,42 @@
+package threadDemos;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.function.Supplier;
+
+public class CompleteableFutureEx1 {
+
+    public static void main(String[] args) throws Exception {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(new ReadFileTask());
+        CompletableFuture<String>resultFuture=future.completeAsync(new AlternateResult());
+        String result= resultFuture.get();
+        System.out.println("result="+result);
+    }
+
+    private static class AlternateResult implements Supplier<String> {
+        @Override
+        public String get() {
+            return "hello-alternate";
+        }
+    }
+
+    private static class ReadFileTask implements Supplier<String> {
+        @Override
+        public String get() {
+            try {
+                System.out.println("****reading file");
+             //   Thread.sleep(10000);
+
+                return "hi-original";
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+}
