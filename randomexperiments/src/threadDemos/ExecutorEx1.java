c56f4b26394b3892cc52e0af96678756831b2db9
vineetsemwal <vineetsemwal82@gmail.com>
2023-10-19 11:56:10 +0530

    future example added in separate class

--- randomexperiments/src/threadDemos/ExecutorEx1.java
+++ randomexperiments/src/threadDemos/ExecutorEx1.java
@@ -11,63 +11,10 @@ public class ExecutorEx1 {
         ExecutorService service = Executors.newFixedThreadPool(2);
         // ExecutorService service=Executors.newCachedThreadPool();
         // ExecutorService service = Executors.newSingleThreadExecutor();
-        //service.execute(new RenderNumbers());
         //ScheduledExecutorService service =Executors.newScheduledThreadPool(2);
         //service.schedule(new RenderNumbers(),10,TimeUnit.SECONDS);
-        Future<List<Employee>>future=service.submit(new FileReaderTask());
-        System.out.println("task finished="+future.isDone());
-        List<Employee>list=future.get();
-        System.out.println("task finished="+future.isDone());
-        System.out.println("fetched employees="+list);
+        service.execute(new RenderNumbers());
         service.shutdown();
-    }
-
-    private static class FileReaderTask implements Callable<List<Employee>> {
-        @Override
-        public List<Employee> call() throws Exception {
-            System.out.println("******reading file");
-            Employee employee1 = new Employee(1, "karthik");
-            Employee employee2 = new Employee(2, "hemanth");
-            List<Employee> list = new ArrayList<>();
-            list.add(employee1);
-            list.add(employee2);
-            return list;
-
-        }
-    }
-
-    protected static class Employee {
-        Employee(int id, String name) {
-            this.id = id;
-            this.name = name;
-        }
-
-        private int id;
-        private String name;
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return "Employee{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    '}';
-        }
     }
 
 
---
+++ randomexperiments/src/threadDemos/FutureEx1.java
@@ -0,0 +1,71 @@
+package threadDemos;
+
+import iodemos.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureEx1 {
+
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService service= Executors.newFixedThreadPool(2);
+        Future<List<Employee>> future=service.submit(new FileReaderTask());
+        System.out.println("task finished="+future.isDone());
+        List<Employee>list=future.get();
+        System.out.println("task finished="+future.isDone());
+        System.out.println("fetched employees="+list);
+    }
+    private static class FileReaderTask implements Callable<List<Employee>> {
+        @Override
+        public List<Employee> call() throws Exception {
+            System.out.println("******reading file");
+            Employee employee1 = new Employee(1, "karthik");
+            Employee employee2 = new Employee(2, "hemanth");
+            List<Employee> list = new ArrayList<>();
+            list.add(employee1);
+            list.add(employee2);
+            return list;
+
+        }
+    }
+
+    protected static class Employee {
+        Employee(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        private int id;
+        private String name;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Employee{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+}
