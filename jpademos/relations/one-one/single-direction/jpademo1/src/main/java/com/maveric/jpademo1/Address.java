1e8d3ef16f80ccf750d017696d9b809898fbe0f7
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-03 15:31:51 +0530

    one-one single , bi directional demos

---
+++ jpademos/relations/one-one/single-direction/jpademo1/src/main/java/com/maveric/jpademo1/Address.java
@@ -0,0 +1,46 @@
+package com.maveric.jpademo1;
+
+import javax.persistence.*;
+
+@Table(name ="trainees_address")
+@Entity
+public class Address {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+    private String city;
+
+    private String building;
+
+    public Address(){}
+
+    public Address(String city,  String building){
+        this.city=city;
+        this.building=building;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+}
---
+++ jpademos/relations/one-one/single-direction/jpademo1/src/main/java/com/maveric/jpademo1/Trainee.java
@@ -0,0 +1,75 @@
+package com.maveric.jpademo1;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Table(name = "trainees_data")
+@Entity
+public class Trainee {
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+    @Column(unique = true,nullable = false)
+    private String name;
+    @Column(name = "dept")
+    private String department;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    public Trainee(){}
+
+    public Trainee( String name, String department,Address address){
+        this.name=name;
+        this.department=department;
+        this.address=address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Trainee trainee = (Trainee) object;
+        return Objects.equals(id, trainee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
