0e08510dd6dc0adaf6a639ca46ca7a605dcf720a
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-09 14:45:10 +0530

    minor change in protype demo

--- designpatterns/src/main/java/com/maveric/designpatterns/protype/PrototypeUse.java
+++ designpatterns/src/main/java/com/maveric/designpatterns/protype/PrototypeUse.java
@@ -3,12 +3,11 @@ package com.maveric.designpatterns.protype;
 public class PrototypeUse {
     public static void main(String[] args) throws CloneNotSupportedException{
         Product product1=new Product("galaxy-g1",10000,"phone","samsung");
-
        Product product2=product1.clone();
        product2.setName("galaxy-g2");
        product2.setPrice(15000);
 
-       if(product1==product2){
+       if(product1!=product2){
            System.out.println("are different objects");
        }
         System.out.println("product1-"+product1.getName()+"-"+product1.getPrice()+"-"+product1.getCategory());
