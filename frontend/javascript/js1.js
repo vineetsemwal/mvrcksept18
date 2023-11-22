07d356fd55833b6e96060bb7eed02dce1849b0e3
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-22 10:51:12 +0530

    object usage

---
+++ frontend/javascript/js1.js
@@ -0,0 +1,9 @@
+var num1=10;
+
+var num2="10";
+
+var equalResult=num1==num2;
+var equalResultWithType=num1===num2;
+
+console.log("result="+equalResult);
+console.log("equal result with type="+equalResultWithType);
---
+++ frontend/javascript/objectuse.js
@@ -0,0 +1,17 @@
+
+var addressDetails={city:"bangalore", state:"karnataka"};
+var pet1={uname:"scooby", age:5,
+
+address:addressDetails
+};
+
+var name=pet1.uname;
+var age=pet1.age;
+
+console.log("name="+name);
+console.log("age="+age);
+
+var city=pet1.address.city;
+var state=pet1.address.state;
+console.log("city="+city);
+console.log("state="+state);
