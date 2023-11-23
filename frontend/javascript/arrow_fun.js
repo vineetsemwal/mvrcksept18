af1ccdf611d87d67b1dbb1cd797c20973128596b
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-23 12:46:24 +0530

    arrow function usage

---
+++ frontend/javascript/arrow_fun.js
@@ -0,0 +1,11 @@
+/*
+arrow function is actually an anonymous function, syntax same as lambda in java
+*/
+const add= (num1, num2)=>{
+    return num1+num2;
+}
+
+
+const result=add(1,2);
+
+console.log("result="+result);
