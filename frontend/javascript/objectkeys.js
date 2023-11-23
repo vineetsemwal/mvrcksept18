bb2a276e8ba619d4e39ea7d4a6e617840cffca5e
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-23 11:19:59 +0530

    fetching field value dynamically

---
+++ frontend/javascript/objectkeys.js
@@ -0,0 +1,15 @@
+/*
+object use fields dynamically
+*/
+
+
+var user = {
+    id: 2,
+    name: "hemanth"
+}
+
+
+var idVal=user['id'];
+var nameVal=user['name']
+console.log("id val="+idVal+"name="+nameVal);
+
