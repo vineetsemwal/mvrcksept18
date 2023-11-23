4c44e1516959abc979c75c90f066d26b3edee036
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-23 10:46:44 +0530

    array demo

---
+++ frontend/javascript/array.js
@@ -0,0 +1,33 @@
+/**
+ * array experiments
+ */
+
+var fruits=["apple", "mango", "grapes"];
+
+const e0=fruits[0];
+
+console.log("element="+e0);
+
+//contains 3 elements where element at index 1 is undefined
+var vegs=["potato",,"tomato",];
+
+
+function printVegs(){
+    for(var i=0;i<vegs.length;i++){
+        var iterated=vegs[i];
+        console.log("iterrated="+iterated);
+    }
+}
+printVegs();
+
+console.log("adding element at a later time");
+
+function addVegetable(vegName){
+    vegs.push(vegName);
+}
+
+addVegetable("brinjal");
+
+printVegs()
+
+
