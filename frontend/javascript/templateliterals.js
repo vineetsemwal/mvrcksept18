9070374dca104d22736a0c3c0b21b3a6d69fd657
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-23 12:54:45 +0530

    template literals

---
+++ frontend/javascript/templateliterals.js
@@ -0,0 +1,15 @@
+/*
+template literals , introduced in es6 for multiline strings, interpolation
+*/
+
+
+
+var str1=`hi
+hello
+how are you`;
+console.log(str1);
+
+var name="kushal";
+var str2=`hello  ${name}, how are you`;
+
+console.log("str2="+str2);
