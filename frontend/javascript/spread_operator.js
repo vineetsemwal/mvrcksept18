131d216e86085e87f617274dafc7071df814f18d
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-27 10:51:29 +0530

    spread operator more demo

--- frontend/javascript/spread_operator.js
+++ frontend/javascript/spread_operator.js
@@ -3,7 +3,9 @@ spread operator
 */
 const product1={name:"samsung-g1", price:20000};
 
-
 const product2={...product1,name:"motorola"};
 
+const product3={...product1};
+
 console.log("product2=",product2);
+console.log("product3=",product3);
