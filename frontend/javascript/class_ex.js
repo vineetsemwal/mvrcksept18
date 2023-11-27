86dd7275e396479db69bde8ee5b660ced1abb8f5
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-27 11:48:23 +0530

    classes demo

--- frontend/javascript/class_ex.js
+++ frontend/javascript/class_ex.js
@@ -13,9 +13,21 @@ class Employee{
         console.log("name="+this.name);
     }
 }
+class Manager extends Employee{
+    
+    constructor(id,name,team){
+        super(id,name);
+          this.team=team;
+    }
+
+}
 
 let emp1=new Employee(1,"ajay");
 let emp2=new Employee(2,"sampath");
 emp1.display();
 emp2.display();
 console.log("objects count="+Employee.count);
+let manager=new Manager(3,"ramya","bank-project");
+console.log("id="+manager.id+"-name="+manager.name+"-team="+manager.team);
+
+
