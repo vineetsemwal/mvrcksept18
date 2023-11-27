c196b2a9b94eb5a5bf6bd90674da1e52da77c2c4
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-27 10:31:38 +0530

    object destructuring- more demo

--- frontend/javascript/object_destructure.js
+++ frontend/javascript/object_destructure.js
@@ -3,7 +3,18 @@
  */
 
 const user={id:1, name :"mahesh"};
-
+//
+//  shortcut of below destructuring syntax
+// const {id:id,name:name}=user;
+ //
 const {id,name}=user;
 
 console.log("id="+id +"- name="+name);
+
+const product={name:"samsung", price:20000};
+//
+// object destructuring
+//
+const{name:nameVal,price:productVal}=product;
+
+console.log("name="+nameVal+"- product="+productVal);
