e2d353b8d73a128ed5e989451668d50841763b72
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-27 12:18:40 +0530

    getter-setter demo

---
+++ frontend/javascript/getters_setters.js
@@ -0,0 +1,28 @@
+/**
+ * getters , setters
+ */
+
+class Employee{
+   _id;
+   _name;
+    constructor(id,name){
+        this._id=id;
+        this._name=name;
+    }
+
+    get id(){
+        console.log("inside getid="+this._id);
+        return this._id;
+    }
+
+    set id(idArg){
+        console.log("inside seetid, arg="+idArg);
+        this._id=idArg
+    }
+
+}
+const emp=new Employee(1,"sampath");
+const id=emp.id;
+console.log("id="+id);
+emp.id=10;
+console.log("emp=",emp);
