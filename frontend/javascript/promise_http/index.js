6f36b5c9f625e0fc2459db263c88e76eb9324fef
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-28 12:21:52 +0530

    node project added

---
+++ frontend/javascript/promise_http/index.js
@@ -0,0 +1,43 @@
+const baseUrl="http://localhost:3000/users";
+const axios=require("axios");
+
+
+/*
+function fetchUserById(id){
+    const url=baseUrl+"/"+id;
+    console.log("url=",url);
+    const promise=axios.get(url);
+    const successFun=(response=>{
+        const user=response.data;
+    console.log("user=",user);
+    });
+    
+    promise.then(successFun);
+}
+*/
+
+async function fetchUserById(id){
+    const url=baseUrl+"/"+id;
+    console.log("url=",url);
+    const promise=axios.get(url);
+    const response=await promise;
+    const user=await response.data;
+    console.log("user=",user);
+}
+
+function fetchAll(){
+    const promise=axios.get(baseUrl);
+    promise.then(response=>{
+        const users=response.data;
+       for(const user of users){
+        console.log("user is",user);
+       }
+    })
+}
+
+console.log("****fetching all");
+fetchAll();
+
+console.log("***fetch by id");
+fetchUserById(1);
+fetchUserById(2);
