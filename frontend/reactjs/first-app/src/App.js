fc5f6e66a2057b70faa5bf93fe6186b63696fd4b
vineetsemwal <vineetsemwal82@gmail.com>
2023-11-30 13:13:25 +0530

    first-app

---
+++ frontend/reactjs/first-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+      <h1>
+    Hello World
+     </h1>
+
+    </div>
+  );
+}
+
+/*
+const element = React.createElement(
+  'h1',
+  {},
+  'Hello, world!');
+
+*/
+
+export default App;
