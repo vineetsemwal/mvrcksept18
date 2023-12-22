5dd6f898c98fab3908cb436b5607433c4e903c13
vineetsemwal <vineetsemwal82@gmail.com>
2023-12-22 11:21:38 +0530

    responseentity demo

--- spring/boot/rest/customerms/src/main/java/com/maveric/customerms/controllers/CustomerController.java
+++ spring/boot/rest/customerms/src/main/java/com/maveric/customerms/controllers/CustomerController.java
@@ -6,6 +6,8 @@ import com.maveric.customerms.dto.CustomerResponse;
 import com.maveric.customerms.service.ICustomerService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,11 +21,21 @@ public class CustomerController {
         this.service = service;
     }
 
+    /*
+    You can return responseentity which contains (response,status,headers)
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> findById(@PathVariable long id)throws Exception{
+        CustomerResponse response= service.findById(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    /*
+
     @GetMapping("/{id}")
     public CustomerResponse findById(@PathVariable long id)throws Exception{
        CustomerResponse response= service.findById(id);
        return response;
-    }
+    }*/
     @PostMapping
     public CustomerResponse register(@RequestBody  CreateCustomerRequest request)throws Exception{
        CustomerResponse response =service.register(request);
